93a19317e0894751f2b4960027e45766e8466bd2
Seemant Kulleen <seemantk@gmail.com>
2016-06-23 22:09:13 -0700

    Use the info from the gist to personalize the page and add form controls for each album, using a bootstrap panel

--- js/deboca.js
+++ js/deboca.js
@@ -13,11 +13,19 @@ var github
 function main(token) {
     github = new GitHub({ token: token.access_token });
     me = github.getUser();
+    me.getProfile()
+        .then(welcome)
+    ;
     me.listGists()
         .then(openCollection)
     ;
 } // main()
 
+function welcome(req) {
+    d3.select(".welcome")
+        .html("Welcome, " + req.data.name)
+    ;
+} // welcome()
 
 function openCollection(list) {
     var mycoll = list.data.filter(function(g) {
@@ -28,31 +36,34 @@ function openCollection(list) {
         var api = github.getGist(mycoll[0].id);
         api.read()
             .then(function(resp) { gist = resp; })
+            .then(renderCollection)
         ;
     }
     else createCollection();
 } // openCollection
 
 function createCollection() {
-    var content = {
-          collection: [{
-              artist: "",
-              album: "",
-              songs: []
-            }],
-        faves: {
-              artists: [],
-              albums: [],
-              songs: []
-            }
-      }
+    var content = [{
+              artist: {
+                    name: ""
+                  , star: false
+                }
+            , album: {
+                    title: ""
+                  , star: false
+                }
+            , songs: [{
+                    title: ""
+                  , star: false
+                }]
+          }]
     ;
     var template = {
           description: "My DeBoCa record collection.",
           public: false,
           files: {
                 "deboca.json": {
-                    content: JSON.stringify(content) 
+                    content: JSON.stringify(content)
                   }
             }
         }
@@ -60,5 +71,73 @@ function createCollection() {
     var api = github.getGist();
     api.create(template)
         .then(function(resp) { gist = resp; })
+        .then(renderCollection)
     ;
 } // createCollection()
+
+
+function renderCollection() {
+    var content = JSON.parse(gist.data.files["deboca.json"].content);
+
+    var record = d3.select(".records").selectAll(".record")
+        .data(content)
+      .enter().append("div")
+        .attr("class", "record col-sm-6 col-md-3")
+    ;
+    record.each(function(d) {
+        console.log(d);
+          var self = d3.select(this);
+
+          var panel = self
+            .append("div")
+              .attr("class", "panel panel-default")
+          ;
+          panel
+            .append("div")
+              .attr("class", "panel-heading")
+            .append("button")
+              .attr("type", "button")
+              .attr("class", "btn btn-primary")
+              .text("Save")
+          ;
+          var group = panel
+            .append("div")
+              .attr("class", "panel-body")
+          ;
+          group
+            .append("input")
+              .attr("type", "text")
+              .attr("class", "artist form-control")
+              .attr("placeholder", "Artist")
+              .attr("value", d.artist.name)
+          ;
+          group
+            .append("input")
+              .attr("type", "text")
+              .attr("class", "album form-control")
+              .attr("placeholder", "Album")
+              .attr("value", d.album.title)
+          ;
+          var tracks = panel
+            .append("div")
+              .attr("class", "panel-footer tracks")
+          ;
+          tracks.selectAll(".track")
+              .data(d.songs, function(o, i) { return i; })
+            .enter().append("input")
+              .attr("type", "text")
+              .attr("class", "track form-control")
+              .attr("placeholder", function(o, i) {
+                  return "Track #" + (i + 1);
+                })
+              .attr("value", function(o) { return o.title; })
+          ;
+          tracks
+            .append("button")
+              .attr("type", "button")
+              .attr("class", "btn btn-primary")
+              .text("Add next track")
+          ;
+      })
+    ;
+} // renderCollection()
